74dd0236be5dd41ef3562e39b49e08c15d438b13
RagingGrim <armandconnor.duplooy@gmail.com>
2017-05-07 10:22:05 +0200

    Added a gcd example.

---
+++ misc/recursion/gcd/gcd.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t gcd_rec(size_t a, size_t b){
+	/*
+	 * So I modified this example from an online source;
+	 * It was returning with the call to itself first
+	 * and with an else handling the base condition.
+	 * 
+	 * I feel this is harder to read and also
+	 * at the very best obfuscates the tail recursion
+	 * ( note : both  examples are optimised to tail recursion )
+	 * */
+	if(b == 0)
+		return a;
+	else
+		return gcd_rec(b, a%b);
+	/*
+	 * Consider two values x and y; Such that
+	 * x is larger than y.
+	 *
+	 * x % y would result in x; This may not seem
+	 * entirely correct but happens to act as an implicit
+	 * if statement.
+	 *
+	 * Should a in this case be smaller than y the paremeters are swapped.
+	 */
+}
+
+size_t gcd(size_t a, size_t b){
+	return gcd_rec(a, b);
+}
+
+int main(){
+	printf("gcd of 4 and 2: %zu\n" ,gcd(4,2));
+	printf("gcd of 12 and 32: %zu\n" ,gcd(12,32));
+
+	return 0;
+}
